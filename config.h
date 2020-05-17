94ed9e5975a70861261909385890893a63a9591d
Francesco Minnocci <ad17fmin@uwcad.it>
2020-05-18 00:08:14 +0200

    only need a few blocks thanks

--- config.h
+++ config.h
@@ -3,8 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	// {"",	"pacpackages",	0,	8},
+	// {"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	{"",	"torrent",	20,	7},
 	/* {"",	"memory",	10,	14}, */
@@ -17,13 +17,8 @@ static const Block blocks[] = {
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	// {"",	"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '|';
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
