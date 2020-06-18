2810767c8a64075e0f4a8e8c8c7d795e7a336aba
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-18 17:02:04 +0200

    changed elements

--- config.h
+++ config.h
@@ -3,11 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"music",	0,	11},
-	// {"",	"pacpackages",	0,	8},
+	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
 	// {"",	"iplocate",	30,	23},
 	/* {"",	"crypto",	0,	13}, */
-	{"",	"price bat \"Basic Attention Token\" 🦁",	0,	20},
+	// {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20},
 	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
 	{"",	"torrent",	20,	7},
@@ -15,9 +15,9 @@ static const Block blocks[] = {
 	// {"",	"cpu",		10,	18},
 	// {"",	"disk",		10,	19},
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
+	// {"",	"weather",	18000,	5},
 	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
+	// {"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
